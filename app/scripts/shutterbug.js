c33978f0e44b7cf9a0a3d48eceb2de2be7165150
Piotr Janik <janikpiotrek@gmail.com>
2014-12-03 23:25:46 +0000

    Refactor the way how the Shutterbug instance is created
    [#83853170]

--- app/scripts/shutterbug.js
+++ app/scripts/shutterbug.js
@@ -1,50 +1,84 @@
+var $ = jQuery;
+var htmlTools      = require('scripts/html-tools');
 var DEFAULT_SERVER = require('scripts/default-server');
 
-var $ = jQuery;
 var MAX_TIMEOUT = 1500;
+var BIN_DATA_SUPPORTED = typeof(window.Blob) === 'function' && typeof(window.Uint8Array) === 'function'; // IE9
 
-// IE9 doesn't implement this.
-var BIN_DATA_SUPPORTED = typeof(window.Blob) === "function" && typeof(window.Uint8Array) === "function";
+// TODO: Construct using opts instead of positional arguments.
+function Shutterbug(selector, imgDst, callback, id, jQuery, opt) {
+  if (typeof(jQuery) != "undefined" && jQuery != null) {
+    $ = jQuery;
+  }
+  // If we still don't have a valid jQuery, try setting it from the global jQuery default.
+  // This can happen if shutterbug.js is included before jquery.js
+  if ((typeof($) == "undefined" || $ == null) && typeof(window.$) != "undefined" && window.$ != null) {
+    $ = window.$;
+  }
 
-var getBaseUrl = function() {
-  var base = window.location.href;
-  return base;
+  opt = opt || {};
+
+  this.server = opt.server || DEFAULT_SERVER;
+  this.imageFormat = opt.format || 'png';
+  this.imageQuality = opt.quality || 1;
+  this.element = selector;
+  this.imgDst = imgDst;
+  this.callback = callback;
+  this.id = id;
+  this.iframeReqTimeout = MAX_TIMEOUT;
+
+  var self = this;
+  var handleMessage = function(message, signature, func) {
+    var data = message.data;
+    if (typeof data === 'string') {
+      try {
+        data = JSON.parse(data);
+        if (data.type === signature) {
+          func(data);
+        }
+      } catch(e) {
+        // Not a json message. Ignore it. We only speak json.
+      }
+    }
+  };
+
+  var htmlFragRequestListen = function(message) {
+    var send_response = function(data) {
+      // Update timeout. When we receive a request from parent, we have to finish nested iframes
+      // rendering in that time. Otherwise parent rendering will timeout.
+      // Backward compatibility: Shutterbug v0.1.x don't send iframeReqTimeout.
+      self.iframeReqTimeout = data.iframeReqTimeout != null ? data.iframeReqTimeout : MAX_TIMEOUT;
+      self.getHtmlFragment(function(html) {
+        var response = {
+          type:        'htmlFragResponse',
+          value:       html,
+          iframeReqId: data.iframeReqId,
+          id:          data.id // return to sender only...
+        };
+        message.source.postMessage(JSON.stringify(response), "*");
+      });
+    };
+    handleMessage(message, 'htmlFragRequest', send_response);
+  };
+
+  var htmlFragResponseListen = function(message) {
+    var send_response = function(data) {
+      if (data.id === self.id) {
+        // Backward compatibility: Shutterbug v0.1.x don't send iframeReqId.
+        var ifeameReqId = data.iframeReqId != null ? data.iframeReqId : 0;
+        self._iframeContentRequests[ifeameReqId].resolve(data.value);
+      }
+    };
+    handleMessage(message, 'htmlFragResponse', send_response);
+  };
+
+  $(document).ready(function () {
+    window.addEventListener('message', htmlFragRequestListen, false);
+    window.addEventListener('message', htmlFragResponseListen, false);
+  });
 };
 
-var _useIframeSizeHack = false;
-var useIframeSizeHack = function(b) {
-  _useIframeSizeHack = b;
-};
-
-var cloneDomItem = function(elem, elemTag) {
-  var width   = elem.width();
-  var height  = elem.height();
-  var returnElm = $(elemTag);
-
-  returnElm.addClass($(elem).attr("class"));
-  returnElm.attr("style", $(elem).attr("style"));
-  returnElm.css('background', $(elem).css("background"));
-  returnElm.attr('width', width);
-  returnElm.attr('height', height);
-  return returnElm;
-};
-
-var generateFullHtmlFromFragment = function (fragment) {
-  return "<!DOCTYPE html>" +
-    "<html>" +
-      "<head>" +
-        "<base href='" + fragment.base_url + "'>" +
-        "<meta content='text/html;charset=utf-8' http-equiv='Content-Type'>" +
-        "<title>content from " + fragment.base_url + "</title>" +
-        fragment.css +
-      "</head>" +
-      "<body>" +
-        fragment.content +
-      "</body>" +
-    "</html>";
-};
-
-var getHtmlFragment = function(callback) {
+Shutterbug.prototype.getHtmlFragment = function(callback) {
   var self = this;
   var $element = $(this.element);
   // .find('iframe').addBack("iframe") handles two cases:
@@ -99,7 +133,7 @@ var getHtmlFragment = function(callback) {
         // When iframe doesn't support Shutterbug, request will timeout and null will be received.
         // In such case just ignore this iframe, we won't be able to render it.
         if (nestedIFrames[i] == null) return;
-        $(iframeElem).attr("src", "data:text/html," + generateFullHtmlFromFragment(nestedIFrames[i]));
+        $(iframeElem).attr("src", "data:text/html," + htmlTools.generateFullHtmlFromFragment(nestedIFrames[i]));
       });
     }
 
@@ -107,7 +141,7 @@ var getHtmlFragment = function(callback) {
     var replacementImgs = $element.find('canvas').addBack('canvas').map(function(i, elem) {
         // Use png here, as it supports transparency and canvas can be layered on top of other elements.
         var dataUrl = elem.toDataURL('image/png');
-        var img = cloneDomItem($(elem), "<img>");
+        var img = htmlTools.cloneDomItem($(elem), "<img>");
         img.attr('src', dataUrl);
         return img;
     });
@@ -121,16 +155,16 @@ var getHtmlFragment = function(callback) {
     }
 
     element.css({
-      'top':0,
-      'left':0,
-      'margin':0,
-      'width':width,
-      'height':height
+      'top': 0,
+      'left': 0,
+      'margin': 0,
+      'width': width,
+      'height' :height
     });
 
     // Due to a weird layout bug in PhantomJS, inner iframes sometimes don't render
     // unless we set the width small. This doesn't affect the actual output at all.
-    if (_useIframeSizeHack) {
+    if (self._useIframeSizeHack) {
       width = 10;
     }
 
@@ -139,7 +173,7 @@ var getHtmlFragment = function(callback) {
       css: css,
       width: width,
       height: height,
-      base_url: getBaseUrl()
+      base_url: window.location.href
     };
 
     $element.trigger('shutterbug-asyouwere');
@@ -148,7 +182,7 @@ var getHtmlFragment = function(callback) {
   });
 };
 
-var getDomSnapshot = function() {
+Shutterbug.prototype.getDomSnapshot = function() {
   // Start timer.
   var self = this;
   var time = 0;
@@ -170,7 +204,7 @@ var getDomSnapshot = function() {
   }
 };
 
-var canvasSnapshot = function() {
+Shutterbug.prototype.canvasSnapshot = function() {
   if (!BIN_DATA_SUPPORTED) {
     return this.basicSnapshot();
   }
@@ -187,10 +221,10 @@ var canvasSnapshot = function() {
   });
 };
 
-function directUpload(options) {
+Shutterbug.prototype.directUpload = function(options) {
   var $canvas = $(this.element);
   var dataURL = $canvas[0].toDataURL('image/' + this.imageFormat, this.imageQuality)
-  var blob = dataURLtoBlob(dataURL);
+  var blob = htmlTools.dataURLtoBlob(dataURL);
   var self = this;
   $.ajax({
     type: 'PUT',
@@ -205,23 +239,7 @@ function directUpload(options) {
   });
 }
 
-function dataURLtoBlob(dataURL) {
-  // Convert base64/URLEncoded data component to raw binary data held in a string.
-  if (dataURL.split(',')[0].indexOf('base64') === -1) {
-    throw new Error('expected base64 data');
-  }
-  var byteString = atob(dataURL.split(',')[1]);
-  // Separate out the mime component.
-  var mimeString = dataURL.split(',')[0].split(':')[1].split(';')[0];
-  // Write the bytes of the string to a typed array.
-  var ia = new Uint8Array(byteString.length);
-  for (var i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-  return new Blob([ia], {type: mimeString});
-}
-
-var basicSnapshot = function() {
+Shutterbug.prototype.basicSnapshot = function() {
   var self = this;
   // Ask for HTML fragment and render it on server.
   this.getHtmlFragment(function(html_data) {
@@ -239,7 +257,7 @@ var basicSnapshot = function() {
   });
 };
 
-var success = function(imageTag) {
+Shutterbug.prototype.success = function(imageTag) {
   if (this.imgDst) {
     $(this.imgDst).html(imageTag);
   }
@@ -247,9 +265,9 @@ var success = function(imageTag) {
     this.callback(msg);
   }
   clearInterval(this.timer);
-}
+};
 
-var fail = function(jqXHR, textStatus, errorThrown) {
+Shutterbug.prototype.fail = function(jqXHR, textStatus, errorThrown) {
   if (this.imgDst) {
     $(this.imgDst).html("snapshot failed");
   }
@@ -259,7 +277,7 @@ var fail = function(jqXHR, textStatus, errorThrown) {
   clearInterval(this.timer);
 }
 
-var requestHtmlFrag = function() {
+Shutterbug.prototype.requestHtmlFrag = function() {
   var destination = $(this.element)[0].contentWindow;
   var message  = {
     type: 'htmlFragRequest',
@@ -268,19 +286,19 @@ var requestHtmlFrag = function() {
   destination.postMessage(JSON.stringify(message), "*");
 };
 
-var htmlSnap = function() {
+Shutterbug.prototype.htmlSnap = function() {
   this.getHtmlFragment(function callback(fragment) {
     // FIXME btoa is not intended to encode text it is for for 8bit per char strings
     // so if you send it a UTF8 string with a special char in it, it will fail
     // this SO has a note about handling this:
     // http://stackoverflow.com/questions/246801/how-can-you-encode-a-string-to-base64-in-javascript
     // also note that btoa is only available in IE10+
-    var encodedContent = btoa(generateFullHtmlFromFragment(fragment));
+    var encodedContent = btoa(htmlTools.generateFullHtmlFromFragment(fragment));
     window.open("data:text/html;base64," + encodedContent);
   });
 };
 
-var imageSnap = function() {
+Shutterbug.prototype.imageSnap = function() {
   var oldImgDst = this.imgDst,
       oldCallback = this.callback,
       self = this;
@@ -295,95 +313,12 @@ var imageSnap = function() {
   this.getDomSnapshot();
 };
 
-var setFailureCallback = function(failCallback) {
+Shutterbug.prototype.setFailureCallback = function(failCallback) {
   this.failCallback = failCallback;
 };
 
-// TODO: Construct using opts instead of positional arguments.
-function Shutterbug(selector, imgDst, callback, id, jQuery, opt) {
-  if (typeof(jQuery) != "undefined" && jQuery != null) {
-    $ = jQuery;
-  }
-  // If we still don't have a valid jQuery, try setting it from the global jQuery default.
-  // This can happen if shutterbug.js is included before jquery.js
-  if ((typeof($) == "undefined" || $ == null) && typeof(window.$) != "undefined" && window.$ != null) {
-    $ = window.$;
-  }
-
-  opt = opt || {};
-
-  var shutterbugInstance = {
-    server: opt.server || DEFAULT_SERVER,
-    element: selector,
-    imgDst: imgDst,
-    callback: callback,
-    id: id,
-    imageFormat: opt.format || "png",
-    imageQuality: opt.quality || 1,
-    getDomSnapshot: getDomSnapshot,
-    basicSnapshot: basicSnapshot,
-    canvasSnapshot: canvasSnapshot,
-    directUpload: directUpload,
-    success: success,
-    fail: fail,
-    getHtmlFragment: getHtmlFragment,
-    requestHtmlFrag: requestHtmlFrag,
-    htmlSnap: htmlSnap,
-    imageSnap: imageSnap,
-    useIframeSizeHack: useIframeSizeHack,
-    iframeReqTimeout: MAX_TIMEOUT,
-    setFailureCallback: setFailureCallback
-  };
-
-  var handleMessage = function(message, signature, func) {
-    var data = message.data;
-    if (typeof data === 'string') {
-      try {
-        data = JSON.parse(data);
-        if (data.type === signature) {
-          func(data);
-        }
-      } catch(e) {
-        // Not a json message. Ignore it. We only speak json.
-      }
-    }
-  };
-
-  var htmlFragRequestListen = function(message) {
-    var send_response = function(data) {
-      // Update timeout. When we receive a request from parent, we have to finish nested iframes
-      // rendering in that time. Otherwise parent rendering will timeout.
-      // Backward compatibility: Shutterbug v0.1.x don't send iframeReqTimeout.
-      shutterbugInstance.iframeReqTimeout = data.iframeReqTimeout != null ? data.iframeReqTimeout : MAX_TIMEOUT;
-      shutterbugInstance.getHtmlFragment(function(html) {
-        var response = {
-          type:        'htmlFragResponse',
-          value:       html,
-          iframeReqId: data.iframeReqId,
-          id:          data.id // return to sender only...
-        };
-        message.source.postMessage(JSON.stringify(response), "*");
-      });
-    };
-    handleMessage(message, 'htmlFragRequest', send_response);
-  };
-
-  var htmlFragResponseListen = function(message) {
-    var send_response = function(data) {
-      if (data.id === shutterbugInstance.id) {
-        // Backward compatibility: Shutterbug v0.1.x don't send iframeReqId.
-        var ifeameReqId = data.iframeReqId != null ? data.iframeReqId : 0;
-        shutterbugInstance._iframeContentRequests[ifeameReqId].resolve(data.value);
-      }
-    };
-    handleMessage(message, 'htmlFragResponse', send_response);
-  };
-
-  $(document).ready(function () {
-    window.addEventListener('message', htmlFragRequestListen, false);
-    window.addEventListener('message', htmlFragResponseListen, false);
-  });
-  return shutterbugInstance;
+Shutterbug.prototype.useIframeSizeHack = function(b) {
+  this._useIframeSizeHack = b;
 };
 
 module.exports = Shutterbug;
